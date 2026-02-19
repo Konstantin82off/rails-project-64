574e0629292038c0b6f69d82d017b9de1cea1527
Konstantin82off <k.udodov@internet.ru>
2026-02-19 12:37:23 +0300

    fix: explicitly exclude dev/test gems on Render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,9 @@
 # exit on error
 set -o errexit
 
+# Явно исключаем гемы из групп development и test
+bundle config set --local without 'development:test'
+
 bundle install
 yarn install
 bundle exec rails assets:precompile
