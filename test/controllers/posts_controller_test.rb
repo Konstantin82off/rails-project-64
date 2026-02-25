7c2406be79ebfefbfaccb0f025dc324e97365b7e
Konstantin82off <k.udodov@internet.ru>
2026-02-25 09:53:41 +0300

    test: all tests passing

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,23 +1,34 @@
+# frozen_string_literal: true
+
 require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get posts_index_url
-    assert_response :success
+  setup do
+    @user = users(:one)
+    @post = posts(:one)
   end
 
-  test "should get show" do
-    get posts_show_url
+  test "should get index" do
+    get posts_url
     assert_response :success
   end
 
   test "should get new" do
-    get posts_new_url
+    sign_in @user
+    get new_post_url
     assert_response :success
   end
 
-  test "should get create" do
-    get posts_create_url
+  test "should create post" do
+    sign_in @user
+    assert_difference("Post.count") do
+      post posts_url, params: { post: { title: "New Post", body: "Body of new post" } }
+    end
+    assert_redirected_to post_url(Post.last)
+  end
+
+  test "should show post" do
+    get post_url(@post)
     assert_response :success
   end
 end
