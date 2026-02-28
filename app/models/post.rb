b814eca95f1b07732aaacbacf175ee40afc1bcee
Konstantin82off <k.udodov@internet.ru>
2026-02-28 11:48:05 +0300

    fix: add creator alias for hexlet tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@
 
 class Post < ApplicationRecord
   belongs_to :user
+  belongs_to :creator, class_name: "User", foreign_key: "user_id"
   belongs_to :category
 
   has_many :post_comments, dependent: :destroy
