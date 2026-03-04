5b105f447dbb5cb43455dc2cba3de22fe9fc11fa
Konstantin82off <k.udodov@internet.ru>
2026-03-04 15:17:03 +0300

    fix: replace alias with explicit methods for user/comments/likes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,23 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
-  belongs_to :creator, class_name: "User", foreign_key: "user_id", inverse_of: :posts
+  belongs_to :creator, class_name: 'User', foreign_key: 'user_id', inverse_of: :posts
   belongs_to :category, inverse_of: :posts
   has_many :post_comments, dependent: :destroy, inverse_of: :post
   has_many :post_likes, dependent: :destroy, inverse_of: :post
-
-  alias user creator
-  alias comments post_comments
-  alias likes post_likes
+  
+  # Явные методы вместо алиасов
+  def user
+    creator
+  end
+  
+  def comments
+    post_comments
+  end
+  
+  def likes
+    post_likes
+  end
 
   validates :title, presence: true
   validates :body, presence: true
