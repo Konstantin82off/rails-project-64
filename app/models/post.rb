4d8fe2ea84d7fbc768a02b9d592ef3d85eb3c3dd
Konstantin82off <k.udodov@internet.ru>
2026-03-04 11:21:27 +0300

    fix models: add aliases for user, likes, comments to pass tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,10 @@ class Post < ApplicationRecord
   has_many :post_comments, dependent: :destroy, inverse_of: :post
   has_many :post_likes, dependent: :destroy, inverse_of: :post
 
+  alias_attribute :user, :creator
+  alias_attribute :comments, :post_comments
+  alias_attribute :likes, :post_likes
+
   validates :title, presence: true
   validates :body, presence: true
 
