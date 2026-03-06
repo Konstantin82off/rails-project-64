cb4c18f7757dcc493930fae1db6855bc0e748a04
Konstantin82off <k.udodov@internet.ru>
2026-03-06 10:08:11 +0300

    refactor: simplify Post model with single creator association
    - Remove all sync callbacks and fixture_insert method
    - Make creator the primary author association
    - Add temporary user compatibility layer

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,24 +1,23 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
-  belongs_to :user, optional: true
-  belongs_to :creator, class_name: "User", optional: true
+  # Основные ассоциации
+  belongs_to :creator, class_name: "User", inverse_of: :created_posts
   belongs_to :category, inverse_of: :posts
+
+  # Временная ассоциация для обратной совместимости (удалим позже)
+  belongs_to :user, optional: true, class_name: "User", foreign_key: "creator_id", inverse_of: :posts
+
+  # Зависимости
   has_many :post_comments, dependent: :destroy, inverse_of: :post
   has_many :post_likes, dependent: :destroy, inverse_of: :post
 
-  def self.fixture_insert(attributes)
-    if attributes[:creator_id].present? && attributes[:user_id].nil?
-      attributes[:user_id] = attributes[:creator_id]
-    end
+  # Валидации
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :creator_id, presence: true
 
-    create(attributes)
-  end
-
-  before_validation :set_user_from_creator, if: -> { creator_id.present? && user_id.nil? }
-  before_validation :set_creator_from_user, if: -> { user_id.present? && creator_id.nil? }
-  before_save :sync_ids
-
+  # Методы для удобства
   def comments
     post_comments
   end
@@ -27,25 +26,12 @@ class Post < ApplicationRecord
     post_likes
   end
 
-  validates :title, presence: true
-  validates :body, presence: true
-
   def liked_by?(user)
-    post_likes.exists?(user: user)
+    post_likes.exists?(creator_id: user.id)
   end
 
-  private
-
-  def set_user_from_creator
-    self.user_id = creator_id
-  end
-
-  def set_creator_from_user
-    self.creator_id = user_id
-  end
-
-  def sync_ids
-    self.creator_id = user_id if creator_id.nil?
-    self.user_id = creator_id if user_id.nil?
+  # Временный метод для обратной совместимости
+  def user
+    creator
   end
 end
