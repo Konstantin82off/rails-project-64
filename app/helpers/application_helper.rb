48da56654a2acc589baa71321cf9a69ecadd42d5
Konstantin82off <k.udodov@internet.ru>
2026-03-17 10:58:39 +0300

    feat: add universal flash messages with helper and partial
    - Create map_alert_type helper for Bootstrap alert classes
    - Create shared/_flash partial for rendering all flash types
    - Update layout to use shared flash partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def map_alert_type(type)
+    {
+      notice: 'info',
+      alert: 'danger',
+      error: 'danger',
+      success: 'success'
+    }.fetch(type.to_sym, 'info')
+  end
 end
