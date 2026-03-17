e147296dd7931670182f16109e92fee240523cc1
Konstantin82off <k.udodov@internet.ru>
2026-03-17 10:47:08 +0300

    fix: update model validations
    - Add orphan_strategy: :restrict to PostComment
    - Add length validations for content and name
    - Change dependent: :destroy in Category
    - Add uniqueness validation for Category name

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,10 +1,11 @@
-# app/models/category.rb
 # frozen_string_literal: true
 
 class Category < ApplicationRecord
-  has_many :posts, dependent: :nullify
+  has_many :posts, dependent: :destroy # Изменено с nullify на destroy
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true,
+                   uniqueness: true,
+                   length: { minimum: 2, maximum: 100 }
 
   def to_s
     name
--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -4,7 +4,8 @@ class PostComment < ApplicationRecord
   belongs_to :user, inverse_of: :comments
   belongs_to :post, inverse_of: :comments
 
-  has_ancestry
+  has_ancestry orphan_strategy: :restrict
 
-  validates :content, presence: true
+  validates :content, presence: true,
+                      length: { minimum: 2, maximum: 1000 }
 end
