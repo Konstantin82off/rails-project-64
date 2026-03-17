a7ccd3b8900c52b148f417ca4b8303df0a2e4fda
Konstantin82off <k.udodov@internet.ru>
2026-03-17 10:51:32 +0300

    refactor: simplify LikesController with find_or_create_by and remove flash messages

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,31 +1,22 @@
-# app/controllers/likes_controller.rb
 # frozen_string_literal: true
 
 class LikesController < ApplicationController
   def create
     authenticate_user!
-
     @post = Post.find(params[:post_id])
-    @like = @post.likes.build(user: current_user)
 
-    if @like.save
-      redirect_to @post, notice: t('.success')
-    else
-      redirect_to @post, alert: @like.errors.full_messages.to_sentence
-    end
+    @like = @post.likes.find_or_create_by(user: current_user)
+
+    redirect_to @post # Без flash-сообщения
   end
 
   def destroy
     authenticate_user!
-
     @post = Post.find(params[:post_id])
-    @like = @post.likes.find_by(user: current_user)
 
-    if @like
-      @like.destroy
-      redirect_to @post, notice: t('.success')
-    else
-      redirect_to @post, alert: t('.not_found')
-    end
+    @like = @post.likes.find_by(user: current_user)
+    @like&.destroy
+
+    redirect_to @post # Без flash-сообщения
   end
 end
