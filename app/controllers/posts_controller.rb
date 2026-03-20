d9610d8f576090e4763c11af3f8ddcd9cdd43841
Konstantin82off <k.udodov@internet.ru>
2026-03-20 21:37:44 +0300

    refactor: add @categories, explicit auth, and move comments query to controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,36 +1,36 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
-  before_action :authenticate_user!, except: %i[index show]
-  before_action :set_post, only: %i[show]
-
   def index
     @posts = Post.includes(:creator, :category).order(created_at: :desc)
   end
 
   def show
+    authenticate_user!
+    @post = Post.find(params[:id])
+    @comments = @post.comments.roots.order(created_at: :desc).includes(:user)
     @comment = PostComment.new
   end
 
   def new
+    authenticate_user!
     @post = Post.new
+    @categories = Category.all
   end
 
   def create
+    authenticate_user!
     @post = current_user.created_posts.build(post_params)
 
     if @post.save
       redirect_to @post, notice: t('.success')
     else
+      @categories = Category.all
       render :new, status: :unprocessable_content
     end
   end
 
   private
-
-  def set_post
-    @post = Post.find(params[:id])
-  end
 
   def post_params
     params.expect(post: %i[title body category_id])
