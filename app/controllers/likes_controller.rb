ccb09288a4a90fb0f03cb33cb6441f63dce75063
Konstantin82off <k.udodov@internet.ru>
2026-03-23 09:00:05 +0300

    refactor: use local variables in LikesController (no instance vars needed in view)

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,20 +3,16 @@
 class LikesController < ApplicationController
   def create
     authenticate_user!
-    @post = Post.find(params[:post_id])
-
-    @like = @post.likes.find_or_create_by(user: current_user)
-
-    redirect_to @post
+    post = Post.find(params[:post_id])
+    post.likes.find_or_create_by(user: current_user)
+    redirect_to post
   end
 
   def destroy
     authenticate_user!
-    @post = Post.find(params[:post_id])
-
-    @like = @post.likes.find_by(user: current_user)
-    @like&.destroy
-
-    redirect_to @post
+    post = Post.find(params[:post_id])
+    like = post.likes.find_by(user: current_user)
+    like&.destroy
+    redirect_to post
   end
 end
